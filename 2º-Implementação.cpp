fdc488a97962c4c39f27e9706bbbadb055c17f35
Rodrigo Zamboni <rodrigo-zamboni@hotmail.com>
2018-09-18 16:20:20 -0300

    2º Implementaçao - básica c/ CrossBlend

--- 2º-Implementação.cpp
+++ 2º-Implementação.cpp
@@ -1,25 +1,23 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-
+#define Ngama 0.01
 #define nmrsEmX 5
 long double a,b;
-
-
-
-
 
 typedef struct individuo{
     float cadeia[nmrsEmX];
 }Individuo;
 
-vector<Individuo> colonia;
 
 Individuo geraX(){
     float HI=b,LO=a;
     Individuo ser;
     for(int i=0;i<nmrsEmX;i++){
         float r = (rand() / (float)RAND_MAX * HI) + LO;
+        char n[20];
+        sprintf(n, "%.3f", r);
+        r = atof(n);
         ser.cadeia[i]=r;
     }
     return ser;
@@ -56,54 +54,67 @@ void mostraPopulacao(vector<Individuo>colonia){
 
 }
 
-/*
-int CrossBlend(int P1,int P2){
 
-    Individuo f1;
-    Individuo f2;
-    float gama = 0.1;
-    float LO = -gama;
-    float HI=gama+1;
+void mutar(Individuo *a){
+    float LO = -Ngama;
+    float HI = Ngama;
     float B = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
-    cout<<gama <<" "<<LO <<" "<<HI <<" "<<B<<" "<<endl;
-    for(int i=0;i<nmrsEmX;++i){
+
+    for(int i=0;i<nmrsEmX;i++){
+        float resp1 = a->cadeia[i]*(1+B);
+
+        char n[20];
+        sprintf(n, "%.3f", resp1);
+        resp1 = atof(n);
+        a->cadeia[i]=resp1;
 
 
-        f1.cadeia[i]=colonia[P1].cadeia[i] + B*(colonia[P2].cadeia[i]-colonia[P1].cadeia[i]);
-        f2.cadeia[i]=colonia[P2].cadeia[i] + B*(colonia[P1].cadeia[i]-colonia[P2].cadeia[i]);
     }
-
-    colonia.push_back(f1);
-    colonia.push_back(f2);
 
 }
 
-*/
-
-void selecao(vector<Individuo> colonia,int tam,Individuo *a,Individuo *b){
+void selecao(vector<Individuo> &colonia,Individuo *a,Individuo *b){
 
     vector<Individuo> filhos;
     Individuo f1,f2;
-    int p2,p1;
+    int p2=0,p1=0;
+
     while(p1==p2){
-        p1=(rand()%tam);
-        p2=(rand()%tam);
+        p1=(rand()%colonia.size());
+        p2=(rand()%colonia.size());
     }
-    float gama = 0.1;
+    float gama = Ngama;
     float LO = -gama;
     float HI=gama+1;
     float B = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
-    cout<<"asdas"<<endl;
 
-    for(int i=0;i<nmrsEmX;++i){
-        f1.cadeia[i]= colonia[p1].cadeia[i] + B*(colonia[p2].cadeia[i]-colonia[p1].cadeia[i]);
-        f2.cadeia[i]= colonia[p2].cadeia[i] + B*(colonia[p1].cadeia[i]-colonia[p2].cadeia[i]);
+    for(int i=0;i<nmrsEmX;i++){
+        float resp1,resp2;
+
+        resp1 = colonia[p1].cadeia[i] + B*(colonia[p2].cadeia[i] - colonia[p1].cadeia[i]);
+        char n[20];
+        sprintf(n, "%.3f", resp1);
+        resp1 = atof(n);
+        f1.cadeia[i]=resp1;
+
+        resp2 = colonia[p2].cadeia[i] + B*(colonia[p1].cadeia[i] - colonia[p2].cadeia[i]);
+        char m[20];
+        sprintf(m, "%.3f", resp2);
+        resp2 = atof(m);
+        f2.cadeia[i]=resp2;
+
     }
-    mostraPopulacao(filhos);
-
+    mutar(&f1);
+    mutar(&f2);
+    colonia.push_back(f1);
+    colonia.push_back(f2);
 }
 
 
+bool meuSort(Individuo a,Individuo b){
+
+    return funcaoY(a)>funcaoY(b);
+}
 
 int main(){
     a=-5.00;
@@ -111,10 +122,9 @@ int main(){
     int nmr;
     cout<<"Digite a qnt de indivíduos da colonia"<<endl;
     cin>>nmr;
-
+    vector<Individuo> colonia;
 
     srand (time(NULL));
-
 
     Individuo p;
     srand (time(NULL));
@@ -125,17 +135,18 @@ int main(){
         colonia.push_back(p);
     }
     mostraPopulacao(colonia);
+    cout<<"Tamanho da colonia Inicial = "<<colonia.size()<<endl;
 
 
-    int g=10;
+
+    int g=3000;
     while(g--){
         Individuo i1,i2;
-        selecao(colonia,colonia.size(),&i1,&i2);
-
-
+        selecao(colonia,&i1,&i2);
+        sort(colonia.begin(),colonia.end(),meuSort);
+        colonia.erase(colonia.begin(),colonia.begin()+2);
     }
-
-
+    mostraPopulacao(colonia);
 
 
 
